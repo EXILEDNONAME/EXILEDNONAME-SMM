6e13d7dcf52749b5d58bf03ad00f42b9baa4d1c2
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2025-06-11 12:14:09 +0700

    Update #30

--- app/Http/Controllers/Backend/__Main/TransactionController.php
+++ app/Http/Controllers/Backend/__Main/TransactionController.php
@@ -79,7 +79,7 @@ class TransactionController extends Controller implements HasMiddleware {
 
     $model = $this->model;
     if (request()->ajax()) {
-      return DataTables::of($this->model::where('id_user', Auth::user()->id)->get())
+      return DataTables::of($this->model::get())
       ->editColumn('date_start', function ($order) { return empty($order->date_start) ? NULL : \Carbon\Carbon::parse($order->date_start)->format('d F Y, H:i'); })
       ->editColumn('date_end', function ($order) { return empty($order->date_end) ? NULL : \Carbon\Carbon::parse($order->date_end)->format('d F Y, H:i'); })
       ->editColumn('description', function ($order) { return nl2br(e($order->description)); })
