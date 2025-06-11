f4f36b621e96d9499ae75e9aeef7582d11747e7c
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2025-06-12 00:00:06 +0700

    Update #48

--- app/Http/Controllers/Backend/__Main/OrderController.php
+++ app/Http/Controllers/Backend/__Main/OrderController.php
@@ -118,7 +118,7 @@ class OrderController extends Controller implements HasMiddleware {
       curl_setopt_array($curl, array(
         CURLOPT_URL => 'https://api.fonnte.com/send', CURLOPT_RETURNTRANSFER => true, CURLOPT_ENCODING => '', CURLOPT_MAXREDIRS => 10, CURLOPT_TIMEOUT => 0, CURLOPT_FOLLOWLOCATION => true, CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1, CURLOPT_CUSTOMREQUEST => 'POST',
         CURLOPT_POSTFIELDS => array('target' => env('APP_NUMBER', ''), 'message' => Auth::user()->name . ' Order ' . $data->name,  'countryCode' => '62'),
-        CURLOPT_HTTPHEADER => array('Authorization: <token>'),
+        CURLOPT_HTTPHEADER => array('Authorization: ' . env('APP_API', '')),
       ));
       curl_exec($curl);
 
