89a1aebf8924eb5466142bd9dfbb2ca2137b4ebe
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2025-06-13 23:25:21 +0700

    Update #77

--- resources/views/pages/backend/__main/wallet/checkout.blade.php
+++ resources/views/pages/backend/__main/wallet/checkout.blade.php
@@ -7,8 +7,6 @@
 
 @section('content')
 <div class="row">
-  <div class="col-xl-3 col-sm-3">
-  </div>
   <div class="col-xl-6 col-sm-6">
     <div class="card card-custom bgi-no-repeat card-stretch gutter-b" style="background-position: right top; background-size: 30% auto; background-image: url({{ env('APP_URL') }}/assets/backend/media/svg/shapes/abstract-4.svg)">
       <div class="card-body">
@@ -35,7 +33,7 @@
       </div>
     </div>
   </div>
-  <div class="col-xl-3 col-sm-3">
+  <div class="col-xl-6 col-sm-6">
   </div>
 </div>
 @endsection
