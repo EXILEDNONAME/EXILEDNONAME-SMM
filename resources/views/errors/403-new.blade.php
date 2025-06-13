4e6c691b2980efdc2310005fef973be6d4e41e4c
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2025-06-13 21:18:53 +0700

    Update #67

--- resources/views/errors/403-new.blade.php
+++ resources/views/errors/403-new.blade.php
@@ -21,7 +21,7 @@
 
   <body id="kt_body" class="header-fixed header-mobile-fixed subheader-enabled subheader-fixed aside-enabled aside-fixed aside-minimize-hoverable page-loading">
     <div class="d-flex flex-column flex-root">
-      <div class="error error-6 d-flex flex-row-fluid bgi-size-cover bgi-position-center" style="background-image: url(/assets/backend/media/error/bg6.jpg);">
+      <div class="error error-6 d-flex flex-row-fluid bgi-size-cover bgi-position-center" style="background-image: url({{ env('APP_URL') }}/assets/backend/media/error/bg6.jpg);">
         <div class="d-flex flex-column flex-row-fluid text-center">
           <h1 class="error-title font-weight-boldest text-white mb-12" style="margin-top: 12rem;"> </h1>
           <p class="display-4 font-weight-bold text-white">
