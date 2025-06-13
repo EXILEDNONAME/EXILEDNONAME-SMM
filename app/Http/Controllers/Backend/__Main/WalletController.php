3849726cff3a26152870ebb974e8bdb54a24588a
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2025-06-13 23:20:00 +0700

    Update #76

--- app/Http/Controllers/Backend/__Main/WalletController.php
+++ app/Http/Controllers/Backend/__Main/WalletController.php
@@ -95,7 +95,7 @@ class WalletController extends Controller {
     $serverKey = config('midtrans.server_key');
     $hashed = hash("sha512", $request->order_id.$request->status_code.$request->gross_amount.$serverKey);
     if($hashed == $request->signature_key){
-      if($request->transaction_status == 'capture') {
+      if($request->transaction_status == 'capture' or $request->transaction_status == 'settlement') {
         $order = WalletTransaction::where('id_order', $request->order_id);
         $order->update(['status' => 'Paid']);
 
